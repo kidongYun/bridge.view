62185464eafe44cbc05c4753de982e3e95909816
kidongYun <oriondigestive@gmail.com>
2020-09-26 22:31:16 +0900

    REDUX

--- src/hooks/useCell.tsx
+++ src/hooks/useCell.tsx
@@ -2,20 +2,53 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../store';
 import { useCallback } from 'react';
 
-import { setCellType, setCellDateTime } from '../store/cell'
+import { 
+    setCellType, 
+    setCellDateTime, 
+    setSubjectId,
+    setSubjectStatus,
+    setSubjectDisplay,
+    setObjectiveTitle,
+    setObjectiveDescription,
+    setObjectivePriority
+ } from '../store/cell'
 
 export default function useCell() {
-    const cellType = useSelector((state: RootState) => state.cell.cellType);
+    const cellType = useSelector((state: RootState) => state.cell);
     const cellDateTime = useSelector((state: RootState) => state.cell.cellDateTime);
+    const subjectId = useSelector((state: RootState) => state.cell.subjectId);
+    const subjectStatus = useSelector((state: RootState) => state.cell.subjectStatus);
+    const subjectDisplay = useSelector((state: RootState) => state.cell.subjectDisplay);
+    const objectiveTitle = useSelector((state: RootState) => state.cell.objectiveTitle);
+    const objectiveDescription = useSelector((state: RootState) => state.cell.objectiveDescription);
+    const objectivePriority = useSelector((state: RootState) => state.cell.objectivePriority);
 
     const dispatch = useDispatch();
-    const onSetCellType = useCallback((cellType: string) => dispatch(setCellType(cellType)), [dispatch]);
-    const onSetCellDateTime = useCallback((cellDateTime: string) => dispatch(setCellDateTime(cellDateTime)), [dispatch]);
+    const onSetCellType = useCallback((type: string) => dispatch(setCellType(type)), [dispatch]);
+    const onSetCellDateTime = useCallback((dateTime: string) => dispatch(setCellDateTime(dateTime)), [dispatch]);
+    const onSetSubjectId = useCallback((id: number) => dispatch(setSubjectId(id)), [dispatch]);
+    const onSetSubjectStatus = useCallback((status: number) => dispatch(setSubjectStatus(status)), [dispatch]);
+    const onSetSubjectDisplay = useCallback((display: string) => dispatch(setSubjectDisplay(display)), [dispatch]);
+    const onSetObjectiveTitle = useCallback((title: string) => dispatch(setObjectiveTitle(title)), [dispatch]);
+    const onSetObjectiveDescription = useCallback((description: string) => dispatch(setObjectiveDescription(description)), [dispatch]);
+    const onSetObjectivePriority = useCallback((priority: number) => dispatch(setObjectivePriority(priority)), [dispatch]);
 
     return {
         cellType,
         cellDateTime,
+        subjectId,
+        subjectStatus,
+        subjectDisplay,
+        objectiveTitle,
+        objectiveDescription,
+        objectivePriority,
         onSetCellType,
-        onSetCellDateTime
+        onSetCellDateTime,
+        onSetSubjectId,
+        onSetSubjectStatus,
+        onSetSubjectDisplay,
+        onSetObjectiveTitle,
+        onSetObjectiveDescription,
+        onSetObjectivePriority
     }
 }
