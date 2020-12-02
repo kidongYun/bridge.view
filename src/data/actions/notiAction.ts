e2681c5c4526cc8b5ca1a28ee8db41b9de2938bd
Kidong Yun <oriondigestive@gmail.com>
2020-12-02 21:31:34 +0900

    NOTI

--- src/data/actions/notiAction.ts
+++ src/data/actions/notiAction.ts
@@ -1,9 +1,11 @@
+import Noti from "../stores/noti";
+
 export const showNotiAction = (
     type: "primary" | "secondary" | "success" | "danger" | "warning" | "info" | "dark" | "light", 
     text: string
 ) => ({
     type: 'SHOW_NOTI_ACTION', 
-    payload: {type, text} 
+    payload: { type, text } 
 });
 
 export const hideNotiAction = () => ({ type: 'HIDE_NOTI_ACTION' });
@@ -12,5 +14,14 @@ type NotiAction =
     | ReturnType<typeof showNotiAction>
     | ReturnType<typeof hideNotiAction>
 
-    
+function noti(state: Noti = new Noti(), action: NotiAction) {
+    switch(action.type) {
+        case 'SHOW_NOTI_ACTION' :
+            return { type: action}
+        case 'HIDE_NOTI_ACTION' :
+            return
+        default :
+            return state;
 
+    }
+}
