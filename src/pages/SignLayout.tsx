79565b8c077a9958b7478ed7527267e03b3a596d
Kidong Yun <oriondigestive@gmail.com>
2021-01-17 15:44:43 +0900

    SignComponent

--- src/pages/SignLayout.tsx
+++ src/pages/SignLayout.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import SignComponent from '../components/organisms/SignComponent'
+import Component from '../components/templates/Component';
 
 interface SignProps {}
 
 const SignLayout: React.FC<SignProps> = () => {
-    let view = <SignComponent/>;
+    let view = 
+    <Component backgroundColor="#eeeeee" backgroundHover="#eeeeee">
+        <Component 
+            width="40%" 
+            height="50%" 
+            backgroundColor="#ffffff" 
+            backgroundHover="#ffffff" 
+            borderRadius="10px">
+            <SignComponent/>
+        </Component>
+    </Component>
 
     return view;
 }
